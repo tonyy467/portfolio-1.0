0ec87de3580f90ea6ea9262dfbdc7d074715f127
Tony Lim <tonylim9438@gmail.com>
2023-09-20 10:16:29 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home.js';
+import Projects from './Projects.js';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/projects" element={<Projects/>}/>
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
